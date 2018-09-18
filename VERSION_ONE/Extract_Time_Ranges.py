f02a12bea7b011b621903a4fac80288436f92af3
Varad Kulkarni <varadkulkarnirules@gmail.com>
2018-09-18 09:48:01 +0530

    Update Extract_Time_Ranges.py

--- VERSION_ONE/Extract_Time_Ranges.py
+++ VERSION_ONE/Extract_Time_Ranges.py
@@ -2,7 +2,10 @@ import wave, struct
 import cv2
 import numpy as np
 import math
-
+import subprocess
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from os import system
+import os
 def bytes_to_int(bytes):
     result = 0
     for b in bytes:
@@ -26,10 +29,9 @@ for i in range(0,length):
     sum+=abs(data)
 avg=sum/length
 avg/=2
-##avg=511.0
-print('average values calculated now finding ranges: ')
-print(avg)
 i=0
+thresh_hold=25*avg
+time_ranges=[]
 while i<=length:
     waveData = waveFile.readframes(1)
     temp=bytes_to_int(waveData)
@@ -42,7 +44,7 @@ while i<=length:
     a=time
     j=i
     ff=0
-    while data>25*avg and j<=length:
+    while data>thresh_hold and j<=length:
         ff=1
         waveData = waveFile.readframes(1)
         temp=bytes_to_int(waveData)
@@ -56,8 +58,37 @@ while i<=length:
     b=i/fps
     t=(math.floor(max(a-10,0)),math.ceil(min(b+10,12600)))
     if ff==1:
-        print(t,file=open("god_attempt10.txt","a"))
+        x,y=t
+        if x<y :
+            time_ranges.append(t)
     i+=1
-
-    
-    
+start_time=[0]*12601
+end_time=[0]*12601
+god=[0]*12601
+max_idx=len(time_ranges)
+for i in range (0,max_idx):
+    x,y=time_ranges[i]
+    start_time[x]+=1
+    end_time[y]+=1
+cur=0
+c=0
+for i in range (0,12601):
+    if start_time[i]!=0:
+        cur+=1
+    if cur!=0:
+        god[i]=1
+    if end_time[i]!=0:
+        cur-=1
+ranges=[]
+i=0
+while i<=12600:
+    j=i
+    f=0
+    while god[j]==1 and j<12600:
+        j+=1
+        f=1
+    if f==1:
+        c+=1
+        ranges.append((i,j-1))
+    i=j
+    i+=1
